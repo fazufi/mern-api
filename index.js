46ce6b186781356317bd480fd0f3ed7ad4e17925
iyan <iyan.cupliz@gmail.com>
2021-03-23 23:17:49 +0700

    sorted array

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
+const PORT = 3002
 const commentsRouter = require('./src/routes/comments')
 app.use(cors())
-
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 
 app.use('/', commentsRouter);
-app.listen(4000);
+app.listen(PORT, () => {
+  console.log(`Server Express sedang berjalan di http://localhost:${PORT}`)
+})
--- src/controllers/comments.js
+++ src/controllers/comments.js
@@ -4,17 +4,19 @@ let comments = require("../db/db.json");
 
 exports.createComments = (req, res, next) => {
   const data = req.body;
-  const c = comments.length + 1;
+  // const c = comments.length + 1;
+  const sorted = comments.sort((a, b) => {
+    console.log(a.id, b.id, a.id - b.id)
+    return a.id - b.id
+  });
+  const c = sorted[sorted.length - 1] ? sorted[sorted.length - 1].id + 1 : 1
   comments.push({ ...data, id: c });
   fs.writeJson(path.join(__dirname, "../db/db.json"), comments);
   res.json(comments);
-
-  next();
 };
 
 exports.getComments = (req, res, next) => {
   res.json(comments);
-  next();
 };
 
 exports.deleteComments = (req, res, next) => {
@@ -29,14 +31,11 @@ exports.deleteComments = (req, res, next) => {
 
 exports.editComments = async (req, res, next) => {
   const data = req.body;
-  const c = comments.length + 1;
-
-  const item = comments.findIndex((v) => v.id == req.params.id);
-  if (item > -1) {
-    // item.updateTo(data)
-    await comments.splice(item, 1);
-    comments.push({ ...data, id:c  });
-    fs.writeJson(path.join(__dirname, "../db/db.json"), comments);
+  const index = comments.findIndex((v) => v.id == req.params.id);
+  if (index > -1) {
+    comments[index] = { ...comments[index], ...data }
+    // comments[index] = Object.assign(comments[index], data)
+    fs.writeJson(path.join(__dirname, "../db/db.json"), comments)
     res.json("successful");
   }
 };
