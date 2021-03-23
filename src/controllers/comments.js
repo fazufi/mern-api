b2c142083c1aacabce10f3db1ccb124ff98ac2af
fazufi <fazufi10@gmail.com>
2021-03-23 20:01:29 +0700

    id put

--- src/controllers/comments.js
+++ src/controllers/comments.js
@@ -1,28 +1,42 @@
-const fs =  require('fs-extra')
-const path = require('path')
-let comments = require('../db/db.json')
+const fs = require("fs-extra");
+const path = require("path");
+let comments = require("../db/db.json");
 
-exports.createComments = (req, res, next)=>{  
-  console.log("iki", req.body.id)
-  const {id}= req.params
-    comments.push(req.body, id)
-    fs.writeJson(path.join(__dirname, '../db/db.json'), comments)
-    res.json(comments)
-  
+exports.createComments = (req, res, next) => {
+  const data = req.body;
+  const c = comments.length + 1;
+  comments.push({ ...data, id: c });
+  fs.writeJson(path.join(__dirname, "../db/db.json"), comments);
+  res.json(comments);
+
   next();
 };
 
-exports.getComments = (req, res, next)=>{  
- 
-  res.json(comments)
-  next()
-}
+exports.getComments = (req, res, next) => {
+  res.json(comments);
+  next();
+};
 
-exports.deleteComments =  (req, res, next) => {
-  const index = comments.findIndex(v => v.id == req.params.id)
-  if (index > -1) {
-    comments.splice(index, 1)
-    fs.writeJson(path.join(__dirname, '../db/db.json'), comments)
-    res.json("successful")
-  
-}}
+exports.deleteComments = (req, res, next) => {
+  const item = comments.findIndex((v) => v.id == req.params.id);
+  if (item > -1) {
+    comments.splice(item, 1);
+    fs.writeJson(path.join(__dirname, "../db/db.json"), comments);
+    res.json("successful");
+  }
+  next();
+};
+
+exports.editComments = async (req, res, next) => {
+  const data = req.body;
+  const c = comments.length + 1;
+
+  const item = comments.findIndex((v) => v.id == req.params.id);
+  if (item > -1) {
+    // item.updateTo(data)
+    await comments.splice(item, 1);
+    comments.push({ ...data, id:c  });
+    fs.writeJson(path.join(__dirname, "../db/db.json"), comments);
+    res.json("successful");
+  }
+};
--- src/routes/comments.js
+++ src/routes/comments.js
@@ -7,5 +7,8 @@ const commentsController= require('../controllers/comments')
 router.post('/comments', commentsController.createComments)
 router.get('/comments', commentsController.getComments)
 router.delete('/comments/:id', commentsController.deleteComments)
+router.put('/comments/:id', commentsController.editComments)
+
+
 
 module.exports= router
